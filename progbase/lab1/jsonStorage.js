33cdfced88d0c52350bce878487358cb7f4f6c94
julia <sivxkxn>
2021-02-02 18:34:47 +0200

    university labs

---
+++ progbase/lab1/jsonStorage.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+//const readLineSync = require('readline-sync')
+class JsonStorage {
+    constructor(filePath) {
+        this.filePath = filePath
+        this.obj = {}
+    }
+    get nextId() {
+        return this.readItems().nextId
+    }
+
+    incrementNextId() {
+        return ++this.obj.nextId
+    }
+
+    readItems() {
+        let jsonText = fs.readFileSync(this.filePath)
+        let module = JSON.parse(jsonText);
+        this.obj = module
+        return this.obj
+    }
+    writeItems(items) {
+        let newModel = {}
+        newModel.nextId = ++this.obj.nextId
+        newModel.items = items
+        const text = JSON.stringify(newModel, null, 4)
+        fs.writeFileSync(this.filePath, text)
+
+    }
+}
+
+module.exports = JsonStorage
---
+++ progbase/lab2/models/course.js
@@ -0,0 +1,12 @@
+class Course {
+
+    constructor(id, name, teacher, hours, credits, beginningDate) {
+        this.id = id  // number
+        this.name=name
+        this.teacher=teacher
+        this.hours=hours
+        this.credits=credits
+        this.beginingDate=beginningDate
+    }
+ };
+ module.exports = Course;
