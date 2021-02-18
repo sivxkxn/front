17489cd59256ed86bcce460fae4e9a937672075e
julia <sivxkxn>
2021-02-19 00:04:30 +0200

    week4

--- coursera/week4/index.js
+++ coursera/week4/index.js
@@ -3,13 +3,51 @@
  * @params {Function[]} – Функции для запроса
  * @returns {Array}
  */
+function sel(collection, fields) {
+    let newCollection = []
+    for (let item of collection) {
+        let tempObj = {}
+        for (let field in item) {
+            if (fields.includes(field)) {
+                tempObj[field] = item[field]
+            }
+        }
+        newCollection.push(tempObj)
+    }
+    return newCollection
+}
+function filt(collection, prop, val) {
+    let newCollection = []
+    // console.log(collection)
+    for (let item of collection) {
+        for (let field in item) {
+            if (field === prop && val.includes(item[field])) {
+                newCollection.push(item)
+            }
+        }
+    }
+    return newCollection
+}
 function query(collection) {
     let arg = [].slice.call(arguments)
     if (arg.length === 1) return arg[0]
     else {
+        commands = []
         for (let i = 1; i < arg.length; i++) {
-
+            commands.push(arg[i])
         }
+        for (let command of commands) {
+            if (command.name === 'filter') {
+                collection = filt(collection, command.field, command.values)
+            }
+        }
+        for (let command of commands) {
+            if (command.name === 'select') {
+                collection = sel(collection, command.property)
+            }
+        }
+
+        return collection
     }
 }
 
@@ -17,7 +55,12 @@ function query(collection) {
  * @params {String[]}
  */
 function select() {
-    return 'it is select'
+    let args = [].slice.call(arguments)
+    let newObj = {
+        name: 'select',
+        property: args
+    }
+    return newObj
 }
 
 /**
@@ -25,7 +68,13 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
-    return 'it is filter'
+    // let args = [].slice.call(arguments)
+    let newObj = {
+        name: 'filter',
+        field: property,
+        values: values
+    }
+    return newObj
 }
 
 let friends = [
@@ -40,11 +89,29 @@ let friends = [
         gender: 'Женский',
         email: 'roachpugh@example.com',
         favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Егор',
+        gender: 'Мужской',
+        email: 'egor@example.com',
+        favoriteFruit: 'Огурец'
+    },
+    {
+        name: 'Вика',
+        gender: 'Мужской',
+        email: 'egor@example.com',
+        favoriteFruit: 'Арбуз'
     }
 ];
-console.log(query(friends))
-// module.exports = {
-//     query: query,
-//     select: select,
-//     filterIn: filterIn
-// };
+
+// console.log(bestFriends = query(
+
+//     friends,
+//     select('name', 'gender', 'email'),
+//     filterIn('favoriteFruit', ['Яблоко', 'Картофель'])
+// ))
+module.exports = {
+    query: query,
+    select: select,
+    filterIn: filterIn
+};
