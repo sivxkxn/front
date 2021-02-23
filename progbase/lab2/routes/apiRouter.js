9a9d971c2b6dca1a8d0ffc78a26b6cde3c518c7f
julia <sivxkxn>
2021-02-23 20:52:29 +0200

    only-front

--- progbase/lab2/routes/apiRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const userRouter = require('../routes/userRouter.js')
-const courseRouter = require('../routes/courseRouter.js')
-const mediaRouter = require('../routes/mediaRouter.js')
-router.use('/users', userRouter)
-router.use('/courses', courseRouter)
-router.use('/media', mediaRouter)
-module.exports = router
--- test.js
+++
@@ -1,6 +0,0 @@
-const user=null
-if(user)
-{
-    console.log('hi')
-}
-else console.log('-')
