304ecab6df8b1f44641ae0ba1b42d6c982cc97aa
sivxkxn <sivxkxn@gmail.com>
2021-04-26 01:58:06 +0300

    week2

--- coursera/course2/week2/exercise/index.js
+++ coursera/course2/week2/exercise/index.js
@@ -5,43 +5,49 @@ module.exports = Collection;
  * @constructor
  */
 function Collection() {
-    this.value = []
-}
-Collection.prototype = Object.create(Array.prototype)
-Collection.prototype.constructor = Collection
-
-// Методы коллекции
-Collection.prototype.values = function () {
-    return this.value()
-}
-Collection.prototype.append = function () {
-
-}
-Collection.prototype.at = function (index) {
-    if (this.indexOf(index + 1)) {
-        return this.indexOf(index + 1)
-    }
-    else
-        return null
-}
+    this.collection = [];
+    Collection.from = function (arr) {
+        let obj = Object.create(Collection.prototype);
+        obj.collection = arr;
+        return obj;
+    };
+};
 Collection.prototype.count = function () {
-    let counter = 0
-    for (let item of this.value) counter++
-    return counter
-}
-Collection.prototype.removeAt = function (index) {
+    return this.collection.length;
+};
+Collection.prototype.values = function () {
+    return this.collection;
+};
+Collection.prototype.at = function (num) {
+    let index = num - 1;
+    let arr = this.values();
+    if (arr[index]) return arr[index];
+    return null;
+};
+Collection.prototype.append = function (arr) {
+    if (arr instanceof Array) {
+        let _this = this;
+        arr.forEach(function (x) {
+            _this.collection.push(x);
+        }
+        )
+    }
+    if (arr instanceof Collection) {
+        let _this = this;
+        arr.collection.forEach(function (x) {
+            _this.collection.push(x);
+        });
+    }
+    else {
+        this.collection.push(arr);
+    }
+};
+Collection.prototype.removeAt = function (pos) {
+    let index = pos - 1;
+    if (this.collection[index]) {
+        this.collection.splice(index, 1);
+        return true;
+    }
+    return false;
 
-}
-// другие методы
-
-
-/**
- * Создание коллекции из массива значений
- */
-Collection.from = function () {
-    let arr = [].slice.apply(arguments)
-
-}
-
-let arr = new Collection
-console.log(arr)
+};
