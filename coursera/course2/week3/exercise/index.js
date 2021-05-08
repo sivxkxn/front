51759d88f8d7673f8bfb677e96d37f21b5544982
Julia Sivokon <sivxkxn@gmail.com>
2021-05-09 00:21:30 +0300

    week3 DONEgit add .git add .!

--- coursera/course2/week3/exercise/index.js
+++ coursera/course2/week3/exercise/index.js
@@ -2,44 +2,20 @@
  * @param {Function[]} operations
  * @param {Function} callback
  */
-// module.exports = 
-function myfunc(operations, callback) {
+module.exports = function (operations, callback) {
     let promises = [];
-    let result = [];
-    let error;
-    function oper(func) {
-        return new Promise(function (resolve, reject) {
-            func(
-                function (err, data) {
-                    if (err !== null) resolve(data);
-                    else reject(err);
-                })
-        })
-    };
 
     for (let i = 0; i < operations.length; i++) {
-        promises[i] = oper(operations[i]);
-   
+        let operation = operations[i];
+        let promise = new Promise(function (resolve, reject) {
+            operation(
+                function (err, data) {
+                    if(err) reject(err);
+                    else resolve(data);
+                })
+        })
+        promises.push(promise);
+
     }
-    return Promise.all(promises).then((data) => callback(null, data)).catch(e => callback(e));
+ Promise.all(promises).then((data) => callback(null, data)).catch(e => callback(e));
 }
-myfunc([
-    // Операция, которая выполняется 500ms
-    function (next) {
-        setTimeout(function () {
-            next(null, '500ms');
-        }, 500);
-    },
-
-    // Операция, которая завершается с ошибкой через 10ms
-
-    // Операция, которая выполняется 200ms
-    function (next) {
-        setTimeout(function () {
-            next(null, '200ms');
-        }, 200);
-    }
-],
-    function (error, results) {
-        console.log(results);
-    })
